f3040754c0232ef4bd822e9fb4fd0d6a544785c8
Tyler Stamm <tyler.stamm1313@gmail.com>
2023-07-13 20:30:44 +0000

    Created next.js navbar

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
-import NavBar from './posts/NavBar.js';
+import NavBar from './posts/NavBar';
 
 export default function Home() {
   return (
@@ -10,56 +10,46 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar></NavBar>
+      {<NavBar></NavBar>}
       <main>
         <h1 className={styles.title}>
-          Read <Link href="/posts/first-post">this page!</Link>
+          Read <Link href="/posts/first-post.js">About Me!</Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Tyler Stamm: Software Developer
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="Projects.html" className={styles.card}>
+            <h3>My Projects &rarr;</h3>
+            <p>Learn about the various projects I've worked on and the code that made it happen!.</p>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Link href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Contact Card &rarr;</h3>
+            <p>Want to Reach Out about a project or just need help? Find everything you need here!</p>
+          </Link>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+          <Link
+            href=""
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <h3>My Blog Posts &rarr;</h3>
+            <p>Discover the journey from Public Ed to Tech. Learn some tech along the way, and some valuable life lessons!</p>
+          </Link>
         </div>
       </main>
 
       <footer>
-        <a
+        <Link
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        </Link>
       </footer>
 
       <style jsx>{`
--- nextjs-blog/pages/posts/NavBar.js
+++ nextjs-blog/pages/posts/NavBar.js
@@ -1,32 +1,37 @@
+import Link from 'next/link';
 
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+                <Link  href= '' className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link href="AboutMe.html" className="nav-link">About Me</Link>
+            </li>
+            <li className="nav-item">
+                <Link href="Projets.html" className="nav-link">Projects</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-var NavBar = React.createClass({
-    render: function() {
-      return (
-  
-        <nav className="navbar navbar-expand-lg navbar-dark">
-          <div className="container">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" href="index.html">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="AboutMe.html">About Me</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="Projects.html">Projects</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#blogsLandingPage">Blogs</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      );
-    }
-  });
+export default NavBar;
+
